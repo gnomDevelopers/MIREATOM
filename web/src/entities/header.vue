828cef910335c6aa2ddaa72e6b4364c7a43e9333
unwelcome <moyapohtaxd@mail.ru>
2024-12-14 14:51:44 +0300

    микрофиксы

--- web/src/entities/header.vue
+++ web/src/entities/header.vue
@@ -6,36 +6,27 @@
     <div class="h-full grid content-center md:ml-16 cursor-pointer">
       <div @click="$router.push({name: 'MainPage'})"><img class="w-8 mb:w-12 h-8 mb:h-12" src="../assets/icons/icon-sigma.svg"/></div>
     </div>
-    <!--  это для норм размера и вкладки для студентоффф-->
-    <!-- <div v-if="isMainPage" class="flex flex-row justify-center items-center gap-4 mt-2">
-      <button class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-hr-color rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300" >Формулы</button>
-      <button class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300" @click="$router.push({name: 'ArticlesPage'})">Статьи</button>
-    </div>
 
-    <div v-if="isArticlesPage" class="flex flex-row justify-center items-center gap-4 mt-2">
-      <button class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300" @click="$router.push({name: 'MainPage'})">Формулы</button>
-      <button class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-hr-color rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300">Статьи</button>
-    </div> -->
     <div class="flex justify-center items-center gap-x-6 ">
       <div
         @click="$router.push({name: 'MyFormulesPage'})"
-        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300"
+        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl text-color border-2 rounded-lg cursor-pointer transition-bg hover:bg-red-50 active:bg-red-100"
         :class="{'border-hr-color': $route.path === '/myformules'}">
         
         <a class="text-xl">Мои формулы</a>
       </div>
       <div
         @click="$router.push({name: 'MainPage'})"
-        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300"
+        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl text-color border-2 rounded-lg cursor-pointer transition-bg hover:bg-red-50 active:bg-red-100"
         :class="{'border-hr-color': $route.path === '/'}">
         
         <a class="text-xl">Формулы</a>
       </div>
       <div
         @click="$router.push({name: 'ArticlesPage'})"
-        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl uus:text-base text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300"
+        class="box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl text-color border-2 rounded-lg cursor-pointer transition-bg hover:bg-red-50 active:bg-red-100"
         :class="{'border-hr-color': $route.path === '/articles'}">
-        
+        <!--box-shadow flex justify-center items-center px-3 mb:px-4 text-xl mb:text-2xl text-color border-2 border-b-gray-600 rounded-lg cursor-pointer transition-bg hover:bg-red-100 active:bg-red-300-->
         <a class="text-xl">Статьи</a>
       </div>
     </div>
--- web/src/shared/myFormulaItem.vue
+++ web/src/shared/myFormulaItem.vue
@@ -5,7 +5,7 @@
         <div class="pb-2 flex justify-center">
           <p class="text-xl">{{ name }}</p>
         </div>
-        <img @click="editFormula" src="@/assets/icons/icon-edit.svg" />
+        <img @click="editFormula" class="cursor-pointer" src="@/assets/icons/icon-edit.svg" />
       </div>
   
       <div class="flex flex-col gap-y-2 bg-gray-200 w-full rounded-lg p-2">
@@ -16,7 +16,7 @@
           <div class="px-2 py-1 flex-grow">
             <p class="text-lg">{{ formula }}</p>
           </div>
-          <div @click="copyFormula" class="btn rounded-r-lg p-1">
+          <div @click="copyFormula" class="btn rounded-r-lg cursor-pointer p-1">
             <svg class="w-8 h-8" viewBox="0 0 40 40" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M33.3333 21.8749L33.3332 7.99979C33.3332 6.34292 31.9901 4.99977 30.3332 4.99979L16.4582 4.99995M23.3333 34.9999L12.0833 35C10.0122 35 8.33325 33.321 8.33325 31.25L8.33325 14.9999C8.33325 12.9289 10.0122 11.2499 12.0832 11.2499L23.3332 11.2499C25.4043 11.2499 27.0832 12.9289 27.0832 14.9999L27.0833 31.2499C27.0833 33.321 25.4043 34.9999 23.3333 34.9999Z" stroke="white" stroke-width="3" stroke-linecap="round"/>
             </svg>
